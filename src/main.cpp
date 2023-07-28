bf65561a3f26b9dbd95fcbbfa0b7f4bddfa1d5db
Mark Albrand <mark.albrand5@gmail.com>
2023-07-28 17:52:15 -0600

    Dennis god

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,84 @@
 #include <iostream>
 #include <glm/glm.hpp>
+#include <SDL.h>
+#include <ctime>
 
-int main() {
-    glm::vec3 vector(1.0f, 0.0f, 0.0f);
-    std::cout << "Hello, World!" << std::endl;
+// Define a Color struct to hold the RGB values of a pixel
+struct Color {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t a;
+};
+
+SDL_Window* window = nullptr;
+SDL_Renderer* renderer = nullptr;
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+Color currentColor = {255, 255, 255, 255}; // Initially set to white
+Color clearColor = {0, 0, 0, 255}; // Initially set to black
+
+
+void init() {
+    SDL_Init(SDL_INIT_VIDEO);
+    window = SDL_CreateWindow("Software Renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+}
+
+void setColor(const Color& color) {
+    currentColor = color;
+}
+
+
+// Function to clear the framebuffer with the clearColor
+void clear() {
+    SDL_SetRenderDrawColor(renderer, clearColor.r, clearColor.g, clearColor.b, clearColor.a);
+    SDL_RenderClear(renderer);
+}
+
+// Function to set a specific pixel in the framebuffer to the currentColor
+void point(int x, int y) {
+    SDL_SetRenderDrawColor(renderer, currentColor.r, currentColor.g, currentColor.b, currentColor.a);
+    SDL_RenderDrawPoint(renderer, x, y);
+}
+
+
+void render() {
+    // Uncomment this line if you want to clear the framebuffer in each frame
+    // clear();
+
+    // Draw a point
+    for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 100; j++) {
+            point(i, j);
+        }
+    }
+
+    // Render the framebuffer to the screen
+}
+
+
+int main(int argc, char** argv) {
+    init();
+    bool running = true;
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+
+        clear();
+        render();
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
