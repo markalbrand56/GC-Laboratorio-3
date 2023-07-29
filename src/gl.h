d6074b7fd65551ce8ff73b481eadc290e87ed289
Mark Albrand <mark.albrand5@gmail.com>
2023-07-28 18:03:24 -0600

    Refactor

---
+++ src/gl.h
@@ -0,0 +1,41 @@
+#pragma once
+
+// Define a Color struct to hold the RGB values of a pixel
+struct Color {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t a;
+};
+
+SDL_Window* window = nullptr;
+SDL_Renderer* renderer = nullptr;
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+Color currentColor = {255, 255, 255, 255}; // Initially set to white
+Color clearColor = {0, 0, 0, 255}; // Initially set to black
+
+
+void init() {
+    SDL_Init(SDL_INIT_VIDEO);
+    window = SDL_CreateWindow("Software Renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+}
+
+void setColor(const Color& color) {
+    currentColor = color;
+}
+
+
+// Function to clear the framebuffer with the clearColor
+void clear() {
+    SDL_SetRenderDrawColor(renderer, clearColor.r, clearColor.g, clearColor.b, clearColor.a);
+    SDL_RenderClear(renderer);
+}
+
+// Function to set a specific pixel in the framebuffer to the currentColor
+void point(int x, int y) {
+    SDL_SetRenderDrawColor(renderer, currentColor.r, currentColor.g, currentColor.b, currentColor.a);
+    SDL_RenderDrawPoint(renderer, x, y);
+}
