12014f509056a5ac8bae847bf5ab6538662a72d2
Mark Albrand <mark.albrand5@gmail.com>
2023-07-28 18:55:10 -0600

    Dibuja algo

--- src/gl.h
+++ src/gl.h
@@ -1,4 +1,11 @@
 #pragma once
+#include <glm/glm.hpp>
+#include <vector>
+#include <iostream>
+#include <array>
+#include <fstream>
+#include <sstream>
+
 
 // Define a Color struct to hold the RGB values of a pixel
 struct Color {
@@ -6,6 +13,10 @@ struct Color {
     uint8_t g;
     uint8_t b;
     uint8_t a;
+};
+
+struct Face {
+    std::vector<std::array<int, 3>> vertexIndices;
 };
 
 SDL_Window* window = nullptr;
@@ -35,7 +46,88 @@ void clear() {
 }
 
 // Function to set a specific pixel in the framebuffer to the currentColor
-void point(int x, int y) {
+void point(const glm::vec3& vertex) {
     SDL_SetRenderDrawColor(renderer, currentColor.r, currentColor.g, currentColor.b, currentColor.a);
-    SDL_RenderDrawPoint(renderer, x, y);
+//    SDL_RenderDrawPoint(renderer, x, y);
+    SDL_RenderDrawPoint(renderer, static_cast<int>(vertex.x), static_cast<int>(vertex.y));
+}
+
+void line(glm::vec3 start, glm::vec3 end) {
+    int x1 = round(start.x), y1 = round(start.y);
+    int x2 = round(end.x), y2 = round(end.y);
+
+    // Draw a line between (x1, y1) and (x2, y2) using point. Bresenham's line algorithm
+    int dx = abs(x2 - x1), dy = abs(y2 - y1);
+    int sx = (x1 < x2) ? 1 : -1, sy = (y1 < y2) ? 1 : -1;
+    int err = dx - dy;
+
+    while (true) {
+        point(glm::vec3(x1, y1, 0.0f));
+        if (x1 == x2 && y1 == y2) break;
+        int e2 = 2 * err;
+        if (e2 > -dy) {
+            err -= dy;
+            x1 += sx;
+        }
+        if (e2 < dx) {
+            err += dx;
+            y1 += sy;
+        }
+    }
+}
+
+void triangle(const glm::vec3& A, const glm::vec3& B, const glm::vec3& C) {
+    line(A, B);
+    line(B, C);
+    line(C, A);
+}
+
+bool loadOBJ(const std::string& path, std::vector<glm::vec3>& out_vertices, std::vector<Face>& out_faces) {
+    // Open the OBJ file
+    std::ifstream file(path);
+    if (!file.is_open()) {
+        std::cerr << "Error opening OBJ file: " << path << std::endl;
+        return false;
+    }
+
+    // Variables to store the vertices and faces from the OBJ file
+    std::vector<glm::vec3> vertices;
+    std::vector<Face> faces;
+
+    // Read the file line by line
+    std::string line;
+    while (std::getline(file, line)) {
+        // Split the line into tokens
+        std::istringstream iss(line);
+        std::string type;
+        iss >> type;
+
+        // Process vertex data
+        if (type == "v") {
+            glm::vec3 vertex;
+            iss >> vertex.x >> vertex.y >> vertex.z;
+            vertices.push_back(vertex);
+        }
+            // Process face data
+        else if (type == "f") {
+            Face face;
+            std::string vertexStr;
+            while (iss >> vertexStr) {
+                std::array<int, 3> vertexIndices;
+                std::replace(vertexStr.begin(), vertexStr.end(), '/', ' ');
+                std::istringstream viss(vertexStr);
+                viss >> vertexIndices[0] >> vertexIndices[1] >> vertexIndices[2];
+                face.vertexIndices.push_back(vertexIndices);
+            }
+            faces.push_back(face);
+        }
+    }
+
+    // Close the file
+    file.close();
+
+    // Return the extracted vertices and faces
+    out_vertices = vertices;
+    out_faces = faces;
+    return true;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
-#include <glm/glm.hpp>
 #include <SDL.h>
 #include "gl.h"
 
 void render() {
-    for (int i = 0; i < 100; i++) {
-        for (int j = 0; j < 100; j++) {
-            point(i, j);
-        }
-    }
+    std::vector<glm::vec3> vertices = {
+            glm::vec3(320.0f, 240.0f, 0.0f),
+            glm::vec3(220.0f, 140.0f, 0.0f),
+            glm::vec3(420.0f, 140.0f, 0.0f)
+    };
+
+    triangle(vertices[0], vertices[1], vertices[2]);
 }
 
 int main(int argc, char** argv) {
     init();
+
+    std::vector<glm::vec3> vertices;
+    std::vector<Face> faces;
+
+    // Load the OBJ file
+    bool success = loadOBJ("../model/quinjet.obj", vertices, faces);
+    if (!success) {
+        std::cerr << "Error loading OBJ file!" << std::endl;
+        return 1;
+    }
+
     bool running = true;
     while (running) {
         SDL_Event event;
@@ -23,7 +35,17 @@ int main(int argc, char** argv) {
         }
 
         clear();
-        render();
+        for (const auto& face : faces) {
+            glm::vec3 A = vertices[face.vertexIndices[0][0] - 1];
+            glm::vec3 B = vertices[face.vertexIndices[1][0] - 1];
+            glm::vec3 C = vertices[face.vertexIndices[2][0] - 1];
+
+            A *= 100.0f;
+            B *= 100.0f;
+            C *= 100.0f;
+
+            triangle(A, B, C);
+        }
 
         SDL_RenderPresent(renderer);
     }
