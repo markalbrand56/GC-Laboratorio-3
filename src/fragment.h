d567e97318977ee53f2fb5d94611c63dbe9d1133
Mark Albrand <mark.albrand5@gmail.com>
2023-08-03 20:36:10 -0600

    Fragments

--- src/fragment.h
+++ src/fragment.h
@@ -1,9 +1,12 @@
 #pragma once
 #include "glm/glm.hpp"
+#include "color.h"
+
 struct Fragment {
 
     glm::ivec2 position; // X and Y coordinates of the pixel (in screen space)
 
     // Other interpolated attributes (e.g., color, texture coordinates, normals) can be added here
+    Color color;
 
 };
