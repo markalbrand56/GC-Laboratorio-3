e2471b20fcfaeb0070810a88981a4c2a0b682de1
Mark Albrand <mark.albrand5@gmail.com>
2023-08-17 12:24:01 -0600

    Refactor

--- src/uniforms.h
+++ src/uniforms.h
@@ -13,11 +13,11 @@ struct Uniforms {
 float a = 3.14f / 3.0f;
 
 glm::mat4 createModelMatrix() {
-    glm::mat4 transtation = glm::translate(glm::mat4(1), glm::vec3(0.0f, 0.0f, 0.0f));
+    glm::mat4 translate = glm::translate(glm::mat4(1), glm::vec3(0.0f, 0.0f, 0.0f));
     glm::mat4 scale = glm::scale(glm::mat4(1), glm::vec3(1.0f, 1.0f, 1.0f));
     glm::mat4 rotation = glm::rotate(glm::mat4(1), glm::radians(a++), glm::vec3(0.0f, 1.0f, 0.0f));
 
-    return transtation * scale * rotation;
+    return translate * scale * rotation;
 }
 
 glm::mat4 createViewMatrix() {
